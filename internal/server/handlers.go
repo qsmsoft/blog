660628161eb66ece7f3a8ef2dd724408bd77cc49
qsmsoft <qsmsoft@gmail.com>
2025-02-20 15:15:20 +0500

    api grouping has been added to server handler

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,5 +1,22 @@
 package server
 
+import (
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
 func (s *Server) MapHandlers() error {
+	v1 := s.echo.Group("/api/v1")
+	health := v1.Group("/health")
+	health.GET("", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, struct {
+			Message string `json:"message"`
+			Status  int    `json:"status"`
+		}{
+			Message: "Healthy",
+			Status:  http.StatusOK,
+		})
+	})
+
 	return nil
 }
