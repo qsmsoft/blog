c5f22412c4f1cf5848110350d568370143bb98d9
qsmsoft <qsmsoft@gmail.com>
2025-02-22 23:44:36 +0500

    handlers has been modified

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -7,8 +7,16 @@ import (
 
 func (s *Server) MapHandlers(e *echo.Echo) error {
 
+	// Init repositories
+	// Init useCases
+	// Init handlers
+
 	v1 := e.Group("/api/v1")
 	health := v1.Group("/health")
+	authGroup := v1.Group("/auth")
+
+	authHttp.MapAuthRoutes(authGroup, authHandlers, mw)
+
 	health.GET("", func(c echo.Context) error {
 		return c.JSON(http.StatusOK, struct {
 			Message string `json:"message"`
