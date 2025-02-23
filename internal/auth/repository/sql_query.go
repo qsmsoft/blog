e599eedb0f4c3cdb3be7c5462d97a3d95e695d46
qsmsoft <qsmsoft@gmail.com>
2025-02-23 18:32:43 +0500

    users sql_query.go has been added

--- internal/auth/repository/sql_query.go
+++ internal/auth/repository/sql_query.go
@@ -1,1 +1,40 @@
 package repository
+
+const (
+	createUserQuery = `INSERT INTO users (first_name, last_name, email, password, role, avatar, created_at, updated_at, login_date)
+							VALUES ($1, $2, $3, $4, COALESCE(NULLIF($5, ''), 'user'), $6, $7, $8, $9)
+							RETURNING *`
+
+	updateUserQuery = `UPDATE users  SET first_name = COALESCE(NULLIF($1, ''), first_name),
+                  			last_name = COALESCE(NULLIF($2, ''), last_name),
+                  			email = COALESCE(NULLIF($3, ''), email),
+                  			role = COALESCE(NULLIF($4, ''), role),
+                  			avatar = COALESCE(NULLIF($5, ''), avatar),
+                  			updated_at = now()
+						WHERE user_id = $7
+						RETURNING *`
+
+	deleteUserQuery = `DELETE FROM users WHERE id = $1`
+
+	getUserQuery = `SELECT id, first_name, last_name, email, role, avatar, created_at, updated_at, login_date  
+					 FROM users 
+					 WHERE id = $1`
+
+	getTotalCount = `SELECT COUNT(id) FROM users WHERE first_name ILIKE '%' || $1 || '%' or last_name ILIKE '%' || $1 || '%'`
+
+	findUsers = `SELECT id, first_name, last_name, email, role, avatar, created_at, updated_at, login_date 
+				  FROM users 
+				  WHERE first_name ILIKE '%' || $1 || '%' or last_name ILIKE '%' || $1 || '%'
+				  ORDER BY first_name, last_name
+				  OFFSET $2 LIMIT $3`
+
+	getTotal = `SELECT COUNT(id) FROM users`
+
+	getUsers = `SELECT id, first_name, last_name, email, role, avatar, created_at, updated_at, login_date
+				 FROM users 
+				 ORDER BY COALESCE(NULLIF($1, ''), first_name) OFFSET $2 LIMIT $3`
+
+	findUserByEmail = `SELECT user_id, first_name, last_name, email, role, avatar,created_at, updated_at, login_date, password
+				 		FROM users 
+				 		WHERE email = $1`
+)
