7e99efa39efa4d26e2cea8276cbbfdd1eddf01b5
qsmsoft <qsmsoft@gmail.com>
2025-02-24 09:16:17 +0500

    csrf.go has been added

---
+++ pkg/csrf/csrf.go
@@ -0,0 +1,31 @@
+package csrf
+
+import (
+	"crypto/sha256"
+	"encoding/base64"
+	"github.com/qsmsoft/blog/pkg/logger"
+	"io"
+)
+
+const (
+	CSRFHeader = "X-CSRF-Token"
+	// 32 bytes
+	csrfSalt = "KbWaoi5xtDC3GEfBa9ovQdzOzXsuVU9I"
+)
+
+// MakeToken creates CSRF token
+func MakeToken(sid string, logger logger.Logger) string {
+	hash := sha256.New()
+	_, err := io.WriteString(hash, csrfSalt+sid)
+	if err != nil {
+		logger.Errorf("Make CSRF Token: %v", err)
+	}
+	token := base64.RawStdEncoding.EncodeToString(hash.Sum(nil))
+	return token
+}
+
+// ValidateToken validates CSRF token
+func ValidateToken(token string, sid string, logger logger.Logger) bool {
+	trueToken := MakeToken(sid, logger)
+	return token == trueToken
+}
