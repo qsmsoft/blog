4f325f3f89dd4f16e70eab59b5b194433736ffd1
qsmsoft <qsmsoft@gmail.com>
2025-02-24 09:17:38 +0500

    users handlers.go has been modified

--- internal/auth/delivery/http/handlers.go
+++ internal/auth/delivery/http/handlers.go
@@ -1,25 +1,33 @@
 package http
 
 import (
+	"bytes"
+	"github.com/google/uuid"
 	"github.com/labstack/echo/v4"
+	"github.com/pkg/errors"
 	"github.com/qsmsoft/blog/config"
 	"github.com/qsmsoft/blog/internal/auth"
 	"github.com/qsmsoft/blog/internal/models"
+	"github.com/qsmsoft/blog/internal/session"
+	"github.com/qsmsoft/blog/pkg/csrf"
 	"github.com/qsmsoft/blog/pkg/httpErrors"
 	"github.com/qsmsoft/blog/pkg/logger"
 	"github.com/qsmsoft/blog/pkg/utils"
+	"io"
+	"mime/multipart"
 	"net/http"
 )
 
 // authHandlers is auth handlers
 type authHandlers struct {
-	cfg    *config.Config
-	authUC auth.UseCase
-	logger logger.Logger
+	cfg       *config.Config
+	authUC    auth.UseCase
+	sessionUC session.SessionUC
+	logger    logger.Logger
 }
 
 // NewAuthHandlers is auth handlers constructor
-func NewAuthHandlers(cfg *config.Config, authUC auth.UseCase, logger logger.Logger) auth.Handlers {
+func NewAuthHandlers(cfg *config.Config, authUC auth.UseCase, sessionUC session.SessionUC, logger logger.Logger) auth.Handlers {
 	return &authHandlers{
 		cfg:    cfg,
 		authUC: authUC,
@@ -29,19 +37,21 @@ func NewAuthHandlers(cfg *config.Config, authUC auth.UseCase, logger logger.Logg
 
 func (h *authHandlers) Register() echo.HandlerFunc {
 	return func(c echo.Context) error {
+		ctx := utils.GetRequestCtx(c)
+
 		user := &models.User{}
 		if err := utils.ReadRequest(c, user); err != nil {
 			utils.LogResponseError(c, h.logger, err)
 			return c.JSON(httpErrors.ErrorResponse(err))
 		}
 
-		createUser, err := h.authUC.Register(c, user)
+		createUser, err := h.authUC.Register(ctx, user)
 		if err != nil {
 			utils.LogResponseError(c, h.logger, err)
 			return c.JSON(httpErrors.ErrorResponse(err))
 		}
 
-		sess, err := h.sessUC.CreateSession(c, &models.Session{
+		sess, err := h.sessionUC.CreateSession(ctx, &models.Session{
 			UserID: createUser.User.ID,
 		}, h.cfg.Session.Expire)
 		if err != nil {
@@ -56,61 +66,262 @@ func (h *authHandlers) Register() echo.HandlerFunc {
 }
 
 func (h *authHandlers) Login() echo.HandlerFunc {
+	type Login struct {
+		Email    string `json:"email" db:"email" validate:"omitempty,lte=60,email"`
+		Password string `json:"password,omitempty" db:"password" validate:"required,gte=6"`
+	}
+
 	return func(c echo.Context) error {
-		return nil
+		ctx := utils.GetRequestCtx(c)
+
+		login := &Login{}
+		if err := utils.ReadRequest(c, login); err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		userWithToken, err := h.authUC.Login(ctx, &models.User{
+			Email:    login.Email,
+			Password: login.Password,
+		})
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		sess, err := h.sessionUC.CreateSession(ctx, &models.Session{
+			UserID: userWithToken.User.ID,
+		}, h.cfg.Session.Expire)
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		c.SetCookie(utils.CreateSessionCookie(h.cfg, sess))
+
+		return c.JSON(http.StatusOK, userWithToken)
 	}
 }
 
 func (h *authHandlers) Logout() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		ctx := utils.GetRequestCtx(c)
+
+		cookie, err := c.Cookie("session-id")
+		if err != nil {
+			if errors.Is(err, http.ErrNoCookie) {
+				utils.LogResponseError(c, h.logger, err)
+				return c.JSON(http.StatusUnauthorized, httpErrors.NewUnauthorizedError(err))
+			}
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(http.StatusInternalServerError, httpErrors.NewInternalServerError(err))
+		}
+
+		if err := h.sessionUC.DeleteByID(ctx, cookie.Value); err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		utils.DeleteSessionCookie(c, h.cfg.Session.Name)
+
+		return c.NoContent(http.StatusOK)
 	}
 }
 
 func (h *authHandlers) Update() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		ctx := utils.GetRequestCtx(c)
+
+		uID, err := uuid.Parse(c.Param("id"))
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		user := &models.User{}
+		user.ID = uID
+
+		if err = utils.ReadRequest(c, user); err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		updatedUser, err := h.authUC.Update(ctx, user)
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		return c.JSON(http.StatusOK, updatedUser)
 	}
 }
 
 func (h *authHandlers) Delete() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		ctx := utils.GetRequestCtx(c)
+
+		uID, err := uuid.Parse(c.Param("id"))
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		if err = h.authUC.Delete(ctx, uID); err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		return c.NoContent(http.StatusOK)
 	}
 }
 
 func (h *authHandlers) GetUserByID() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		ctx := utils.GetRequestCtx(c)
+
+		uID, err := uuid.Parse(c.Param("id"))
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		user, err := h.authUC.GetByID(ctx, uID)
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		return c.JSON(http.StatusOK, user)
 	}
 }
 
 func (h *authHandlers) FindByName() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		ctx := utils.GetRequestCtx(c)
+
+		if c.QueryParam("name") == "" {
+			utils.LogResponseError(c, h.logger, httpErrors.NewBadRequestError("name is required"))
+			return c.JSON(http.StatusBadRequest, httpErrors.NewBadRequestError("name is required"))
+		}
+
+		paginationQuery, err := utils.GetPaginationFromCtx(c)
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		response, err := h.authUC.FindByName(ctx, c.QueryParam("name"), paginationQuery)
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		return c.JSON(http.StatusOK, response)
 	}
 }
 
 func (h *authHandlers) GetUsers() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		ctx := utils.GetRequestCtx(c)
+
+		paginationQuery, err := utils.GetPaginationFromCtx(c)
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		usersList, err := h.authUC.GetUsers(ctx, paginationQuery)
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		return c.JSON(http.StatusOK, usersList)
 	}
 }
 
 func (h *authHandlers) GetMe() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		user, ok := c.Get("user").(*models.User)
+		if !ok {
+			utils.LogResponseError(c, h.logger, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
+			return utils.ErrResponseWithLog(c, h.logger, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
+		}
+
+		return c.JSON(http.StatusOK, user)
 	}
 }
 
 func (h *authHandlers) UploadAvatar() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		ctx := utils.GetRequestCtx(c)
+
+		bucket := c.QueryParam("bucket")
+		uID, err := uuid.Parse(c.Param("id"))
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		image, err := utils.ReadImage(c, "file")
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		file, err := image.Open()
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+		defer func(file multipart.File) {
+			err := file.Close()
+			if err != nil {
+				h.logger.Fatalf("failed to close file: %v", err)
+			}
+		}(file)
+
+		binaryImage := bytes.NewBuffer(nil)
+		if _, err = io.Copy(binaryImage, file); err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		contentType, err := utils.CheckImageFileContentType(binaryImage.Bytes())
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		reader := bytes.NewReader(binaryImage.Bytes())
+
+		updatedUser, err := h.authUC.UploadAvatar(ctx, uID, models.UploadInput{
+			File:        reader,
+			Name:        image.Filename,
+			Size:        image.Size,
+			ContentType: contentType,
+			BucketName:  bucket,
+		})
+		if err != nil {
+			utils.LogResponseError(c, h.logger, err)
+			return c.JSON(httpErrors.ErrorResponse(err))
+		}
+
+		return c.JSON(http.StatusOK, updatedUser)
 	}
 }
 
 func (h *authHandlers) GetCSRFToken() echo.HandlerFunc {
 	return func(c echo.Context) error {
-		return nil
+		sid, ok := c.Get("id").(string)
+		if !ok {
+			utils.LogResponseError(c, h.logger, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
+			return utils.ErrResponseWithLog(c, h.logger, httpErrors.NewUnauthorizedError(httpErrors.Unauthorized))
+		}
+		token := csrf.MakeToken(sid, h.logger)
+		c.Response().Header().Set(csrf.CSRFHeader, token)
+		c.Response().Header().Set("Access-Control-Expose-Headers", csrf.CSRFHeader)
+
+		return c.NoContent(http.StatusOK)
 	}
 }
