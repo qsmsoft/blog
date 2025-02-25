98f3a3a03ad464d2a483c3d2bf48d8431cd8bc7e
qsmsoft <qsmsoft@gmail.com>
2025-02-25 18:51:55 +0500

    debug and request_logger middlewares have been added

---
+++ internal/middleware/debug.go
@@ -0,0 +1,22 @@
+package middleware
+
+import (
+	"fmt"
+	"github.com/labstack/echo/v4"
+	"net/http"
+	"net/http/httputil"
+)
+
+// DebugMiddleware dumps request middleware
+func (mw *MiddlewareManager) DebugMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if mw.cfg.Server.Debug {
+			dump, err := httputil.DumpRequest(c.Request(), true)
+			if err != nil {
+				return c.NoContent(http.StatusInternalServerError)
+			}
+			mw.logger.Info(fmt.Sprintf("\nRequest dump begin :--------------\n\n%s\n\nRequest dump end :--------------", dump))
+		}
+		return next(c)
+	}
+}
