327c79c0dde759ce3bb50d3ded916c62642ca38d
qsmsoft <qsmsoft@gmail.com>
2025-02-25 18:55:39 +0500

    main.go configured

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,9 +4,12 @@ import (
 	"github.com/jmoiron/sqlx"
 	"github.com/qsmsoft/blog/config"
 	"github.com/qsmsoft/blog/internal/server"
+	"github.com/qsmsoft/blog/pkg/db/aws"
 	"github.com/qsmsoft/blog/pkg/db/postgres"
+	rClient "github.com/qsmsoft/blog/pkg/db/redis"
 	"github.com/qsmsoft/blog/pkg/logger"
 	"github.com/qsmsoft/blog/pkg/utils"
+	"github.com/redis/go-redis/v9"
 	"log"
 	"os"
 )
@@ -45,7 +48,22 @@ func main() {
 		}
 	}(psqlDB)
 
-	s := server.NewServer(cfg, psqlDB, appLogger)
+	redisClient := rClient.NewRedisClient(cfg)
+	defer func(redisClient *redis.Client) {
+		err := redisClient.Close()
+		if err != nil {
+			appLogger.Fatalf("Redis close: %s", err)
+		}
+	}(redisClient)
+	appLogger.Info("Redis connected")
+
+	awsClient, err := aws.NewAWSClient(cfg.AWS.Endpoint, cfg.AWS.MinioAccessKey, cfg.AWS.MinioSecretKey, cfg.AWS.UseSSL)
+	if err != nil {
+		appLogger.Errorf("AWS Client init: %s", err)
+	}
+	appLogger.Info("AWS S3 connected")
+
+	s := server.NewServer(cfg, psqlDB, redisClient, awsClient, appLogger)
 	if err = s.Run(); err != nil {
 		log.Fatal(err)
 	}
