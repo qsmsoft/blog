d662a3237f41cba3003defd678c4ff59805ba889
qsmsoft <qsmsoft@gmail.com>
2025-02-25 18:50:37 +0500

    redis config has been modified

--- internal/auth/redis_repository.go
+++ internal/auth/redis_repository.go
@@ -1,3 +1,12 @@
 package auth
 
-type RedisRepository struct{}
+import (
+	"context"
+	"github.com/qsmsoft/blog/internal/models"
+)
+
+type RedisRepository interface {
+	GetByIDCtx(ctx context.Context, key string) (*models.User, error)
+	SetUserCtx(ctx context.Context, key string, seconds int, user *models.User) error
+	DeleteUserCtx(ctx context.Context, key string) error
+}
--- internal/auth/repository/redis_repository.go
+++ internal/auth/repository/redis_repository.go
@@ -1,1 +1,57 @@
 package repository
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/pkg/errors"
+	"github.com/qsmsoft/blog/internal/auth"
+	"github.com/qsmsoft/blog/internal/models"
+	"github.com/redis/go-redis/v9"
+	"time"
+)
+
+// Auth redis repository
+type authRedisRepo struct {
+	redisClient *redis.Client
+}
+
+// NewAuthRedisRepository is auth redis repository constructor
+func NewAuthRedisRepository(redisClient *redis.Client) auth.RedisRepository {
+	return &authRedisRepo{redisClient: redisClient}
+}
+
+// GetByIDCtx gets user by id
+func (a *authRedisRepo) GetByIDCtx(ctx context.Context, key string) (*models.User, error) {
+	userBytes, err := a.redisClient.Get(ctx, key).Bytes()
+	if err != nil {
+		return nil, errors.Wrap(err, "authRedisRepo.GetByIDCtx.redisClient.Get")
+	}
+	user := &models.User{}
+	if err = json.Unmarshal(userBytes, user); err != nil {
+		return nil, errors.Wrap(err, "authRedisRepo.GetByIDCtx.json.Unmarshal")
+	}
+
+	return user, nil
+}
+
+// SetUserCtx caches user with duration in seconds
+func (a *authRedisRepo) SetUserCtx(ctx context.Context, key string, seconds int, user *models.User) error {
+	userBytes, err := json.Marshal(user)
+	if err != nil {
+		return errors.Wrap(err, "authRedisRepo.SetUserCtx.json.Unmarshal")
+	}
+	if err = a.redisClient.Set(ctx, key, userBytes, time.Second*time.Duration(seconds)).Err(); err != nil {
+		return errors.Wrap(err, "authRedisRepo.SetUserCtx.redisClient.Set")
+	}
+
+	return nil
+}
+
+// DeleteUserCtx deletes user by key
+func (a *authRedisRepo) DeleteUserCtx(ctx context.Context, key string) error {
+	if err := a.redisClient.Del(ctx, key).Err(); err != nil {
+		return errors.Wrap(err, "authRedisRepo.DeleteUserCtx.redisClient.Del")
+	}
+
+	return nil
+}
